5c77cbba2011248deb68051f5d40b50aa8c12f02
Autumn Lay <autumn.lay0@gmail.com>
2021-11-15 09:22:10 -0700

    buttons

--- src/components/Thread.vue
+++ src/components/Thread.vue
@@ -2,6 +2,8 @@
   <div class="thread row">
     <!-- only render this IF account.id (account having an id indicates someone is loggedin) -->
     <!-- <CreatePost v-if="account.id" /> -->
+    <!-- <button class="btn btn-primary" @click="newer">Newer</button>
+    <button class="btn btn-primary" @click="older">Older</button> -->
     <div v-for="p in posts" :key="p.id" class="col-12">
       <Post :post="p" />
     </div>
@@ -16,6 +18,14 @@ export default {
       posts: computed(() => AppState.posts),
       account: computed(() => AppState.account),
       profile: computed(() => AppState.profile),
+      // newer(){
+      //  // score.value = false
+      //  // name.value = true
+      // }
+      // older(){
+      //   //score.value = true
+      //   //name.value = false
+      // }
     };
   },
 };
